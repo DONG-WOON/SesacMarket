c98f4f615bc76ab934a21091bdd4c6ab9c8f2822
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-09-08 18:39:07 +0900

    [update] Extension+UIView
    - View의 rounded, makeBorder, backgroundColor 기능 추가

--- SesacMarket/Extension/Extension+UIView.swift
+++ SesacMarket/Extension/Extension+UIView.swift
@@ -8,4 +8,22 @@
 import UIKit.UIView
 
 extension UIView: Identifiable { }
+extension UIView {
+    
+    func rounded(radius: CGFloat = 10) {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+    }
+    
+    func makeBorder(width: CGFloat, color: UIColor) {
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = width
+    }
+    
+    convenience init(_ backgroundColor: UIColor) {
+        self.init()
+        
+        self.backgroundColor = backgroundColor
+    }
+}
 
