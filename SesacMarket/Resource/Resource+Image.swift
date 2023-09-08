365edc811af53dead5c254d186268aa4a60e27c4
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-09-08 18:40:47 +0900

    [chore] Image(Namespace) 추가

---
+++ SesacMarket/Resource/Resource+Image.swift
@@ -0,0 +1,14 @@
+//
+//  Resource+Image.swift
+//  SesacMarket
+//
+//  Created by 서동운 on 9/8/23.
+//
+
+import Foundation
+
+enum Image {
+    static let search = "magnifyingglass"
+    static let wish = "heart"
+    static let wishFill = "heart.fill"
+}
